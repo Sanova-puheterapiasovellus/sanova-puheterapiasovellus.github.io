7d39fefa26e7a898de3a724bfc56f87b4ebb5ae9
Leevi Koponen <contact@leevikoponen.fi>
2025-10-09 14:09:14 +0300

    Initial work on notification system
    Reinstates a slightly evolved version of the component base class.
    
    Also had to add the iterable DOM TS definitions that I forgot were still
    separate when originally setting up.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-// @ts-expect-error seemingly doesn't understand bundlers?
 import "./index.css";
 import { initializeCategorySelector } from "./components/categories.ts";
 import { initializeGameContainer } from "./components/game.ts";
 import { initializeHeader } from "./components/navbar.ts";
+import { initializeNotificationSystem } from "./components/notifications.ts";
 import { initializeSyllablePlayer } from "./components/syllables.ts";
 
 initializeHeader();
 initializeCategorySelector();
 initializeGameContainer();
 initializeSyllablePlayer();
+initializeNotificationSystem();
