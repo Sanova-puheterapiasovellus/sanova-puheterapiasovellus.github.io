16dcb0354aad4e44a1a3bb5a11428909706432d7
Otto Mäkitalo <otto.m@outlook.com>
2025-11-10 10:46:40 +0000

    Fix close dialog close button

--- src/components/imageCredits.ts
+++ src/components/imageCredits.ts
@@ -3,7 +3,7 @@ import "./styles/imageCredits.css";
 
 const imageCredits = expectElement("image-credits-text", HTMLElement);
 const imageCreditsDialog = expectElement("image-credits", HTMLDialogElement);
-const closeButton = expectElement("image-credits-close", HTMLElement);
+const closeButton = expectElement("image-credits-close", HTMLButtonElement);
 
 /**
  * Open the credits dialog with text.
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { dispatchInternalNotification } from "./common/events.ts";
 import { initializeAllWords } from "./components/allwords.ts";
 import { initializeCategorySelector } from "./components/categories.ts";
 import { initializeGameContainer } from "./components/game.ts";
+import { initializeImageCreditsDialog } from "./components/imageCredits.ts";
 import { initializeHeader } from "./components/navbar.ts";
 import { initializeNotificationSystem } from "./components/notifications.ts";
 import { initializeSyllablePlayer } from "./components/syllables.ts";
@@ -18,5 +19,6 @@ initializeSyllablePlayer();
 initializeSyllableHintPlayer();
 initializeAllWords(route);
 initializeNotificationSystem();
+initializeImageCreditsDialog();
 
 dispatchInternalNotification(document.body, "Testi-ilmoitus", "Lisätietoja");
