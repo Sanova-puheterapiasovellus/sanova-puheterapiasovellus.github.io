d3f1486a449a215abb21a5897e0513add72793cd
Lilja Vesa <lilja.vesa7@gmail.fi>
2025-11-10 16:01:33 +0200

    Fixed formatting

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "@playwright/test";
 
 test("dummy test", async ({ page }) => {
-    await page.goto('about:blank');
+    await page.goto("about:blank");
     expect(true).toBe(true);
 });
