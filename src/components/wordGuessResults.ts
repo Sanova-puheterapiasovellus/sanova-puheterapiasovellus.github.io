a21a53634576c164eb23ca87d90c73ffb73535c1
quiteelight <quiteelighti@gmail.com>
2025-11-10 19:27:54 +0200

    Remove comments + remove word duplicates

--- src/components/wordGuessResults.ts
+++ src/components/wordGuessResults.ts
@@ -1,70 +1,3 @@
-/*import { expectElement } from "../common/dom";
-import type { GameSession } from "./GameSession";
-import "./styles/word-guess-results.css";
-import { unlockPageScroll } from "../common/preventScroll.ts";
-
-const resultsDialog = expectElement("word-guess-results-dialog", HTMLDialogElement);
-const resultsCloseButton = expectElement("word-guess-results-close", HTMLButtonElement);
-const replayIncorrectButton = expectElement("word-guess-replay", HTMLButtonElement);
-
-function handleDialogClose(_: Event): void {
-    resultsDialog.close();
-    unlockPageScroll();
-}
-
-export function showWordGuessResults(gameSession: GameSession): void {
-    resultsCloseButton.addEventListener("click", handleDialogClose);
-
-    // Handler here to get access to the gameSession
-    function handleReplayIncorrect(_: Event): void {
-        const words: string[] = gameSession.getIncorrectlyGuessedWords();
-        if (words.length > 0) {
-            // Update the gameSession
-            dispatchEvent(
-                new CustomEvent("words-selected", {
-                    bubbles: true,
-                    detail: {
-                        selections: words.map((w, idx) => ({ name: w, index: idx })),
-                        category: null,
-                        isReplay: true,
-                    },
-                }),
-            );
-
-            // Trigger the game start
-            dispatchEvent(
-                new CustomEvent("word-selected", {
-                    bubbles: true,
-                    detail: { name: null, index: 0 },
-                }),
-            );
-        }
-    }
-
-    if (gameSession.getIncorrectlyGuessedWords().length === 0) {
-        // Hide the replay button, no words to replay
-        replayIncorrectButton.classList.add("hidden");
-    } else {
-        // Show the replay button
-        replayIncorrectButton.classList.remove("hidden");
-    }
-
-    replayIncorrectButton.addEventListener("click", handleReplayIncorrect);
-
-    const correctAnswerP = expectElement("correct-answers", HTMLParagraphElement);
-    const letterHintsP = expectElement("letter-hints-used", HTMLParagraphElement);
-    const vocalHintsP = expectElement("vocal-hints-used", HTMLParagraphElement);
-    const textHintsP = expectElement("text-hints-used", HTMLParagraphElement);
-
-    correctAnswerP.textContent = `Oikeita vastauksia: ${gameSession.getCorrectAnswerCount()} / ${gameSession.getTotalWordCount()}`;
-    letterHintsP.textContent = `Käytettyja kirjainvihjeitä: ${gameSession.getLetterHintsUsed()}`;
-    vocalHintsP.textContent = `Käytettyja äänivihjeitä: ${gameSession.getVocalHintsUsed()}`;
-    textHintsP.textContent = `Käytettyja tekstivihjeitä: ${gameSession.getTextHintsUsed()}`;
-
-    resultsDialog.showModal();
-}
-*/
-
 import { expectElement } from "../common/dom";
 import type { GameSession } from "./GameSession";
 import "./styles/word-guess-results.css";
@@ -129,6 +62,9 @@ function handleReplay(gameSession: GameSession): void {
     if (checkboxIncorrect.checked) words.push(...gameSession.getIncorrectlyGuessedWords());
     if (words.length === 0) return;
 
+    // Remove duplicates
+    const uniqueWords = [...new Set(words)];
+
     // Remove result dialog immediately
     // when starting a new game
     handleDialogClose(new Event("click"));
@@ -138,7 +74,7 @@ function handleReplay(gameSession: GameSession): void {
         new CustomEvent("words-selected", {
             bubbles: true,
             detail: {
-                selections: words.map((w, idx) => ({ name: w, index: idx })),
+                selections: uniqueWords.map((w, idx) => ({ name: w, index: idx })),
                 category: null,
                 isReplay: true,
             },
