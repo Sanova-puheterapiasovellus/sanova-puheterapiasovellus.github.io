e1deca4cfc7296c6d7ef4c29b71e013c63ce202c
Lilja Vesa <lilja.vesa7@gmail.fi>
2025-11-18 16:15:18 +0200

    Migrated to section format instead of dialog for allwords view and styled it

--- src/components/allwords.ts
+++ src/components/allwords.ts
@@ -6,15 +6,20 @@ import type { FilterOptions } from "./searchAndFilter";
 import { setupSearchAndFilter } from "./searchAndFilter";
 import styles from "./styles/allWords.module.css";
 
-const dialog = expectElement("all-words-dialog", HTMLDialogElement);
+const categoriesPage = expectElement("categories-page", HTMLElement);
+const allWordsPage = expectElement("all-words-page", HTMLElement);
+const allWordsHeader = expectElement("all-words-header", HTMLElement);
+const mainviewHeader = expectElement("mainview", HTMLElement);
+
 const allWordsList = expectElement("all-words-list", HTMLUListElement);
 const filtersSection = expectElement("filters-section", HTMLElement);
 const searchContainer = expectElement("search-container", HTMLElement);
 const categoriesContainer = expectElement("category-filters", HTMLElement);
 const collapseCategories = expectElement("collapse-categories", HTMLElement);
 
-dialog.className = styles.dialog;
+allWordsPage.className = styles.page;
 allWordsList.className = styles.list;
+allWordsHeader.className = styles.header;
 filtersSection.className = styles.filters;
 categoriesContainer.className = styles.categoryFilters;
 searchContainer.className = styles.searchContainer;
@@ -26,13 +31,6 @@ collapseCategories.addEventListener("click", function () {
         categoriesContainer.style.maxHeight = "";
     } else {
         categoriesContainer.style.maxHeight = `100%`;
-    }
-});
-
-dialog.addEventListener("click", (e) => {
-    if (e.target === dialog) {
-        dialog.close();
-        window.location.hash = "#";
     }
 });
 
@@ -107,7 +105,19 @@ function renderAllImages(filters: FilterOptions) {
     });
 }
 export function initializeAllWords(hash: Store<string>) {
-    hash.filter((value) => value === "#search").subscribe((_) => dialog.showModal());
+    hash.filter((value) => value === "#search").subscribe((_) => {
+        categoriesPage.classList.add("hidden");
+        allWordsPage.classList.remove("hidden");
+        allWordsList.scrollTop = 0;
+        mainviewHeader.style.paddingTop = `0px`;
+    });
+
+    hash.filter((value) => value === "" || value === "#").subscribe(() => {
+        categoriesPage.classList.remove("hidden");
+        allWordsPage.classList.add("hidden");
+        mainviewHeader.style.paddingTop = `90px`;
+    });
+
     const categoryData = wordsData.categories.map((category) => ({
         name: category.name,
         imagePath: getImagePath(category.image),
