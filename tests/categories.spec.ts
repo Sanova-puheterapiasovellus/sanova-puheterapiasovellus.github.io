9657d1c06c7d69224b3ab626ccb12d9c655aba9c
Eero Holmala <eero.holmala@tuni.fi>
2025-11-22 21:22:33 +0200

    Fix clothes categories test

--- tests/categories.spec.ts
+++ tests/categories.spec.ts
@@ -6,7 +6,16 @@ test("Test Clothes Categories", async ({ page }) => {
     await page.getByText("Vaatteet").first().click();
     const wordList = wordsData.categories.find((p) => p.name === "vaatteet")?.words;
 
-    while (await page.getByText("Sanaharjoittelu").first()) {
+    let count = 1;
+    const numWords = parseInt(
+        (await page.locator("#word-guess-progress-counter").textContent()).substring(2),
+        10,
+    );
+
+    await expect(numWords).toBe(wordList?.length);
+
+    //
+    while (count <= numWords) {
         const imgSrcText = await page.getAttribute("#word-guess-image", "src");
         expect(imgSrcText).toContain("/assets/");
         expect(wordList).not.toBeUndefined();
@@ -15,8 +24,17 @@ test("Test Clothes Categories", async ({ page }) => {
         expect(word).not.toBeNull();
         await page.locator("#hidden-input").fill(word);
         await page.locator("#word-guess-submit").click();
-        await expect(page.locator(".letter-slot").first()).toHaveText("_");
+        if (count <= numWords - 1) {
+            await expect(page.locator(".letter-slot").first()).toHaveText("_");
+        }
+        count++;
     }
+
+    await expect(page.locator("#correct-answers")).toHaveText("Oikeita vastauksia: 14 / 14");
+    await expect(page.locator("#correct-with-hints")).toBeHidden();
+    await expect(page.locator("#letter-hints-used")).toHaveText("Käytettyja kirjainvihjeitä: 0");
+    await expect(page.locator("#text-hints-used")).toHaveText("Käytettyja kuvailevia vihjeitä: 0");
+    await expect(page.locator("#vocal-hints-used")).toHaveText("Käytettyja tavuvihjeitä: 0");
     await page.locator("#word-guess-results-close").click();
 
     await page.close();
