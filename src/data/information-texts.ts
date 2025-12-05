658dddeca1c73898a39bb70da4fe815ae391d0c3
Otto Mäkitalo <otto.m@outlook.com>
2025-12-05 18:33:19 +0000

    Change other texts to more accessible version

--- src/data/information-texts.ts
+++ src/data/information-texts.ts
@@ -1,24 +1,24 @@
 export const SANOVA_INFORMATION = `
-<p>Tämä sovellus on tarkoitettu henkilöille, joilla on afasia ja/tai nimeämisen vaikeuksia. Afasia on puheen ymmärtämisen ja/tai tuottamisen häiriö, joka johtuu useimmiten aivovauriosta.</p>
-<br>
-<p>Sanova-sovellus perustuu SFA (Semantic Feature Analysis) ja PCT (Phonological Cueing Therapy) –menetelmiin. SFA on nimeämisharjoittelumenetelmä, jonka ideana on parantaa kykyä nimetä sanoja semanttisia, eli sanan merkityksiin liittyviä piirteitä, harjoittelemalla. PCT puolestaan on menetelmä, joka keskittyy nimeämisen harjoitteluun äänteellisten tai ortografisten eli sanojen kirjainasuun liittyvien vihjeiden avulla.</p>
-<br>
-<p>Sovellus parantaa sananlöytämistä, tukee itsenäistä harjoittelua ja mahdollistaa harjoittelun paineettomasti omassa rauhassa. Sovellusta on helppo käyttää ja se sisältää arkipäiväisiä, hyödyllisiä sanoja.</p>
+<p class="information-paragraph">Tämä sovellus on tarkoitettu henkilöille, joilla on afasia ja/tai nimeämisen vaikeuksia. Afasia on puheen ymmärtämisen ja/tai tuottamisen häiriö, joka johtuu useimmiten aivovauriosta.</p>
+
+<p class="information-paragraph">Sanova-sovellus perustuu SFA (Semantic Feature Analysis) ja PCT (Phonological Cueing Therapy) –menetelmiin. SFA on nimeämisharjoittelumenetelmä, jonka ideana on parantaa kykyä nimetä sanoja semanttisia, eli sanan merkityksiin liittyviä piirteitä, harjoittelemalla. PCT puolestaan on menetelmä, joka keskittyy nimeämisen harjoitteluun äänteellisten tai ortografisten eli sanojen kirjainasuun liittyvien vihjeiden avulla.</p>
+
+<p class="information-paragraph">Sovellus parantaa sananlöytämistä, tukee itsenäistä harjoittelua ja mahdollistaa harjoittelun paineettomasti omassa rauhassa. Sovellusta on helppo käyttää ja se sisältää arkipäiväisiä, hyödyllisiä sanoja.</p>
 `;
 
 export const SANOVA_INFORMATION_SOURCES = `Duodecim Terveyskirjasto, 2023; Evans ym., 2021; Fridriksson ym., 2018; Greenwood ym., 2010; Hashimoto, 2023; Quique ym., 2019; Roelofs, 2021`;
 export const SANOVA_INFORMATION_SOURCES_DETAILED = `
-<p>Evans, W. S., Cavanaugh, R., Gravier, M. L., Autenreith, A. M., Doyle, P. J., Hula, W. D., & Dickey, M. W. (2021). Effects of Semantic Feature Type, Diversity, and Quantity on Semantic Feature Analysis Treatment Outcomes in Aphasia. American Journal of Speech-Language Pathology, 30(1S), 344–358. https://doi.org/10.1044/2020_AJSLP-19-00112</p>
-<br>
-<p>Fridriksson, J., den Ouden, D.-B., Hillis, A. E., Hickok, G., Rorden, C., Basilakos, A., Yourganov, G., & Bonilha, L. (2018). Anatomy of aphasia revisited. Brain (London, England : 1878), 141(3), 848–862. https://doi.org/10.1093/brain/awx363</p>
-<br>
-<p>Greenwood, A., Grassly, J., Hickin, J., & Best, W. (2010). Phonological and orthographic cueing therapy: A case of generalised improvement. Aphasiology, 24(9), Article 924635765. https://doi.org/10.1080/02687030903168220</p>
-<br>
-<p>Hashimoto, N. (2023). Using a combined working memory – Semantic feature analysis approach to treat anomia in aphasia: A Pilot Study. Journal of Communication Disorders, 106, Article 106384. https://doi.org/10.1016/j.jcomdis.2023.106384</p>
-<br>
-<p>Quique, Y. M., Evans, W. S., & Dickey, M. W. (2019). Acquisition and Generalization Responses in Aphasia Naming Treatment: A Meta-Analysis of Semantic Feature Analysis Outcomes. American Journal of Speech-Language Pathology, 28(1S), 230–246. https://doi.org/10.1044/2018_AJSLP-17-0155</p>
-<br>
-<p>Roelofs, A. (2021). Phonological cueing of word finding in aphasia: insights from simulations of immediate and treatment effects. Aphasiology, 35(2), 169–185. https://doi.org/10.1080/02687038.2019.1686748</p>
+<p class="information-paragraph">Evans, W. S., Cavanaugh, R., Gravier, M. L., Autenreith, A. M., Doyle, P. J., Hula, W. D., & Dickey, M. W. (2021). Effects of Semantic Feature Type, Diversity, and Quantity on Semantic Feature Analysis Treatment Outcomes in Aphasia. American Journal of Speech-Language Pathology, 30(1S), 344–358. https://doi.org/10.1044/2020_AJSLP-19-00112</p>
+
+<p class="information-paragraph">Fridriksson, J., den Ouden, D.-B., Hillis, A. E., Hickok, G., Rorden, C., Basilakos, A., Yourganov, G., & Bonilha, L. (2018). Anatomy of aphasia revisited. Brain (London, England : 1878), 141(3), 848–862. https://doi.org/10.1093/brain/awx363</p>
+
+<p class="information-paragraph">Greenwood, A., Grassly, J., Hickin, J., & Best, W. (2010). Phonological and orthographic cueing therapy: A case of generalised improvement. Aphasiology, 24(9), Article 924635765. https://doi.org/10.1080/02687030903168220</p>
+
+<p class="information-paragraph">Hashimoto, N. (2023). Using a combined working memory – Semantic feature analysis approach to treat anomia in aphasia: A Pilot Study. Journal of Communication Disorders, 106, Article 106384. https://doi.org/10.1016/j.jcomdis.2023.106384</p>
+
+<p class="information-paragraph">Quique, Y. M., Evans, W. S., & Dickey, M. W. (2019). Acquisition and Generalization Responses in Aphasia Naming Treatment: A Meta-Analysis of Semantic Feature Analysis Outcomes. American Journal of Speech-Language Pathology, 28(1S), 230–246. https://doi.org/10.1044/2018_AJSLP-17-0155</p>
+
+<p class="information-paragraph">Roelofs, A. (2021). Phonological cueing of word finding in aphasia: insights from simulations of immediate and treatment effects. Aphasiology, 35(2), 169–185. https://doi.org/10.1080/02687038.2019.1686748</p>
 `;
 
 export const VIEW_INSTRUCTIONS = `
@@ -26,25 +26,30 @@ adsjsadnjsdabnkdsbkjhdsabbsdhj
 `;
 
 export const GAME_INSTRUCTIONS = `
+<div class="information-content-block">
 <h3>Tavoite:</h3>
 <p>Kirjoita kuvassa näkyvän asian nimi oikein</p>
-<br>
+</div>
 
+<div class="information-content-block">
 <h3>Pelaaminen:</h3>
 <p>1. Kirjoita vastaus, mitä kuvassa on</p>
 <p>2. Paina vastaa, peli näyttää värin(vihreä oikein/punainen väärin) ja siirtyy
 automaattisesti seuraavaan sanaan</p>
 <p>3. Jos et tiedä sanaa, paina ohita sana ja peli siirtyy seuraavaan sanaan</p>
-<br>
+</div>
 
+<div class="information-content-block">
 <h3>Vihjeet:</h3>
 <p>1. Kirjainvihje lisää yhden oikean kirjaimen sanaan joka painalluksella</p>
 <p>2. Tavuvihje soittaa tavuja sanan alusta alkaen, lisäten uuden tavun jokaisella vihjeen painalluksella
 ja kun kaikki tavut on soitettu, soittaa vihje koko sanan seuraavilla painalluksilla</p>
 <p>3. Kuvaileva vihje näyttää kuvailevan tekstivihjeen sanasta</p>
-<br>
+</div>
 
+<div class="information-content-block">
 <h3>Tulossivu:</h3>
 <p>Pelin jälkeen voit tarkastella, miten hyvin osasit sanat ja valita uudelleen harjoiteltavaksi
 ohitetut, väärin menneet tai vihjeillä ratkaistut sanat tai niiden yhdistelmän</p>
+</div>
 `;
