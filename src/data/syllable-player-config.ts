07b620860078131ed63fd25406642d141849b89a
Leevi Koponen <contact@leevikoponen.fi>
2025-12-09 16:37:07 +0200

    Reverb and loudness normalization to syllable playback
    I definitely don't know what I'm doing in the slightest, but it's maybe
    a bit better with these changes?

--- src/data/syllable-player-config.ts
+++ src/data/syllable-player-config.ts
@@ -1,7 +1,18 @@
+// The basics of how sounds are combined with each other.
 export const characterSeparation = 0;
 export const syllableSeparation = 0.25;
 export const startOffset = 0;
 export const edgeFade = 0.1;
+
+// Options for reverb applied to smooth playback.
+export const reverbWetGain = 0.15;
+export const reverbDuration = 0.12;
+export const reverbDecay = 3;
+
+// Target linear root mean square loudness. (-20 dBFS)
+export const targetLoudness = 10 ** (-20 / 20);
+
+// Options for compressor to smooth playback.
 export const compressorOptions = {
     threshold: -22,
     knee: 24,
